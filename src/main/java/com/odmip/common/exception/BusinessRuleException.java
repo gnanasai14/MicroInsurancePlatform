5b80187f85d17a3abe6c8f5855a8f512c17b8398
gnana <gnanasai369@gmail.com>
2026-07-17 15:49:46 -0400

    Commit

--- src/main/java/com/odmip/common/exception/BusinessRuleException.java
+++
@@ -1,7 +0,0 @@
-package com.odmip.common.exception;
-
-public class BusinessRuleException extends RuntimeException {
-    public BusinessRuleException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/odmip/common/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.odmip.common.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
